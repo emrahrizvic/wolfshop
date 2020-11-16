becf2fab6c8451224ef1450d0316ee1196f055ae
Emrah Rizvic <emrah_rizvic@hotmail.com>
2020-11-16 22:57:46 +0100

    User Auth endpoint.

--- backend/server.js
+++ backend/server.js
@@ -4,6 +4,7 @@ import colors from 'colors';
 import connectDB from './config/db.js';
 
 import productRoutes from './routes/productRoutes.js';
+import userRoutes from './routes/userRoutes.js';
 import { errorHandler, notFound } from './middleware/errorMiddleware.js';
 
 dotenv.config();
@@ -12,11 +13,15 @@ connectDB();
 
 const app = express();
 
+// This allow as to have json in the body
+app.use(express.json());
+
 app.get('/', (req, res) => {
-  res.send('API is running...');
+    res.send('API is running...');
 });
 
 app.use('/api/products', productRoutes);
+app.use('/api/users', userRoutes);
 
 app.use(notFound);
 
@@ -24,6 +29,6 @@ app.use(errorHandler);
 
 const PORT = process.env.PORT || 5000;
 app.listen(
-  5000,
-  console.log(`Server running in ${process.env.NODE_ENV} on port ${PORT}`)
+    5000,
+    console.log(`Server running in ${process.env.NODE_ENV} on port ${PORT}`)
 );
